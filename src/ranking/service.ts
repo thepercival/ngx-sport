c31e9c1e3def68d4502b938afb0d7fae97c7f406
thepercival <coendunnink@gmail.com>
2020-06-08 19:42:49 +0200

    BREAKING CHANGE(core); make stable

--- src/ranking/service.ts
+++ src/ranking/service.ts
@@ -116,6 +116,7 @@ export class RankingService {
             // if (nrOfIterations > this.maxPlaces) {
             //     console.error('should not be happening for ranking calc');
             //     break;
+            // quit
             // }
         }
         return rankedItems;
