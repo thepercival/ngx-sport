16e045fc94917581fe7e1f8b6673997504f59e15
thepercival <coendunnink@gmail.com>
2022-07-19 15:04:50 +0200

    fix(startloc): remove categoryNr

--- src/competitor/json.ts
+++ src/competitor/json.ts
@@ -3,7 +3,6 @@ import { JsonStartLocation } from "./startLocation/json";
 export interface JsonCompetitor extends JsonStartLocation {
     name: string;
     id: string | number;
-    categoryNr: number;
     registered?: boolean;
     info?: string;
 }
